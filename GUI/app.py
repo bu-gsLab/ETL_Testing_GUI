cba55ada0df9efc6034b1467604a12cb97bf35ad
Robert Vitale <bobby21@bu.edu>
2026-03-17 13:22:18 -0400

    Created fork for tamalero and now importing the forked repo as submodule

--- GUI/app.py
+++ GUI/app.py
@@ -46,6 +46,7 @@ class MainWindow(QMainWindow):
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+    app.setStyle("Fusion") 
     app.setWindowIcon(QIcon(str(gui_dir / "icon.png")))
     window = MainWindow()
     window.show()
