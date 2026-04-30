8746585611ed83ac1a3f7756ddca46fac2d01bb2
rovitale <bobby21@bu.edu>
2026-04-30 12:26:42 -0400

    bugfix udev script

--- scripts/load_udev.sh
+++ scripts/load_udev.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-if [ -f udev/99-ETL_Testing_GUI.rules ] then
-    sudo cp udev/99-ETL_Testing_GUI.rules /etc/udev/rules.d/
-    sudo udevadm control --reload
-    sudo udevadm trigger
-fi
+if [ -f "$PROJECT_BASE/udev/99-ETL_Testing_GUI.rules" ]; then
+    sudo cp "$PROJECT_BASE/udev/99-ETL_Testing_GUI.rules" /etc/udev/rules.d/
