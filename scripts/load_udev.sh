f5cd0a73685d974281ce9e0aa6d0801cec5c740a
rovitale <bobby21@bu.edu>
2026-04-30 12:29:30 -0400

    reload udev

--- scripts/load_udev.sh
+++ scripts/load_udev.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 if [ -f "$PROJECT_BASE/udev/99-ETL_Testing_GUI.rules" ]; then
     sudo cp "$PROJECT_BASE/udev/99-ETL_Testing_GUI.rules" /etc/udev/rules.d/
+    sudo udevadm control --reload
+    sudo udevadm trigger
 fi
